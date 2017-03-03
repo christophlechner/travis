b6913a27a52dc269c0d715b5660f64d5590beed4
Christoph <Christoph@DESKTOP-2VMGRA3.fritz.box>
2017-03-03 22:26:42 +0100

    add travis.yml

--- src/main/java/com/theduke/travis/Sample.java
+++ src/main/java/com/theduke/travis/Sample.java
@@ -12,7 +12,7 @@ package com.theduke.travis;
 public class Sample {
     
     public static void main(String[] args) {
-        System.out.println("com.theduke.travis.Sample.main()");
+        System.out.println("com.theduke.travis.Sample.main() ok");
     }
     
 }
