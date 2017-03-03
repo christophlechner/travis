c8672ce010813bf9df245bec3e0e9d6e08b99d09
Christoph <Christoph@DESKTOP-2VMGRA3.fritz.box>
2017-03-03 22:15:19 +0100

    init

---
+++ src/main/java/com/theduke/travis/Sample.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.theduke.travis;
+
+/**
+ *
+ * @author Christoph
+ */
+public class Sample {
+    
+    public static void main(String[] args) {
+        System.out.println("com.theduke.travis.Sample.main()");
+    }
+    
+}
